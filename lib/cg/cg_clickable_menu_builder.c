ebd356dae70d6742e9ff04e04a2039c75fa04f8b
alexandre.descoins <alexandre.descoins@epitech.eu>
2020-02-06 17:20:58 +0100

    [ADD]: lib cg plus its header file

---
+++ lib/cg/cg_clickable_menu_builder.c
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2019
+** cg_check_clock.c
+** File description:
+** Check a clock
+*/
+
+#include <SFML/Graphics.h>
+
+//cg_select_menu_t *cg_select_menu_builder(cg_select_settings_t *comp, sfVector2f pos)
